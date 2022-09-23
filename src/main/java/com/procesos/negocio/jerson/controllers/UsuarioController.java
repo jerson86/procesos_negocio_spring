3673c9f2f643509b0cdc5af9bbb86e0b25fe5f14
SCIS3-PC01\SCIS3-PC01 <prueba>
2022-09-22 21:30:15 -0500

    Persistencia jpa usuario

--- src/main/java/com/procesos/negocio/jerson/controllers/UsuarioController.java
+++ src/main/java/com/procesos/negocio/jerson/controllers/UsuarioController.java
@@ -1,25 +1,87 @@
 package com.procesos.negocio.jerson.controllers;
 
 import com.procesos.negocio.jerson.models.Usuario;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.procesos.negocio.jerson.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UsuarioController {
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @GetMapping(value = "/usuario/{id}")
-    public Usuario getUsuario(@PathVariable Long id){
-        Usuario usuario = new Usuario();
-        usuario.setId(id);
-        usuario.setNombre("Jerson");
-        usuario.setApellidos("garcia");
-        usuario.setDocumento("12345678");
-        usuario.setDireccion("ssssss");
-        usuario.setFechaNacimiento(new Date(2020,12,01));
-        usuario.setTelefono("22222222");
+    public Optional<Usuario> getUsuario(@PathVariable Long id){
+        Optional<Usuario> usuario= usuarioRepository.findById(id);
         return usuario;
     }
+
+    @PostMapping("/usuario")
+    public Usuario crearUsuario(@RequestBody Usuario usuario){
+        usuarioRepository.save(usuario);
+        return usuario;
+    }
+
+    @GetMapping("/usuarios")
+    public List<Usuario> listarUsuarios(){
+        return usuarioRepository.findAll();
+    }
+
+    @GetMapping("/usuario/{nombre}/{apellidos}")
+    public List<Usuario> listarPorNombreApellidos(@PathVariable String nombre,
+                                                  @PathVariable String apellidos){
+        return usuarioRepository.findAllByNombreAndApellidos(nombre, apellidos);
+    }
+
+    @GetMapping("/usuario/apellidos/{apellidos}")
+    public List<Usuario> listarPorApellidos(@PathVariable String apellidos){
+        return usuarioRepository.findAllByApellidos(apellidos);
+    }
+
+    @GetMapping("/usuario/nombre/{nombre}")
+    public List<Usuario> listarPorNombre(@PathVariable String nombre){
+        return usuarioRepository.findAllByNombre(nombre);
+    }
+    @PutMapping("/usuario/{id}")
+    public Usuario editarUsuario(@PathVariable Long id,
+                                           @RequestBody Usuario usuario){
+        Usuario usuarioBD = usuarioRepository.findById(id).get();
+        try{
+            usuarioBD.setNombre(usuario.getNombre());
+            usuarioBD.setApellidos(usuario.getApellidos());
+            usuarioBD.setDireccion(usuario.getDireccion());
+            usuarioBD.setDocumento(usuario.getDocumento());
+            usuarioBD.setFechaNacimiento(usuario.getFechaNacimiento());
+            usuarioBD.setTelefono(usuario.getTelefono());
+            usuarioRepository.save(usuarioBD);
+            return usuarioBD;
+        }catch(Exception e){
+            return null;
+        }
+    }
+
+    @DeleteMapping("/usuario/{id}")
+    public Usuario eliminarUsuario(@PathVariable Long id){
+        Usuario usuarioBD = usuarioRepository.findById(id).get();
+        try{
+            usuarioRepository.delete(usuarioBD);
+            return usuarioBD;
+        }catch (Exception e){
+            return null;
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/procesos/negocio/jerson/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.procesos.negocio.jerson.repository;
+
+import com.procesos.negocio.jerson.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    List<Usuario> findAllByNombre(String nombre);
+    List<Usuario> findAllByApellidos(String apellidos);
+    List<Usuario> findAllByNombreAndApellidos(String nombre, String apellidos);
+}
